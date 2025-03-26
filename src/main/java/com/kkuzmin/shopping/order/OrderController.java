3e0bbbebf66452af262053c25fa659b7840d920f
Konstantin Kuzmin <kocc95@mail.ru>
2025-03-26 16:07:31 +0100

    API docs configured and endpoints documentation written

--- src/main/java/com/kkuzmin/shopping/order/OrderController.java
+++ src/main/java/com/kkuzmin/shopping/order/OrderController.java
@@ -1,5 +1,8 @@
 package com.kkuzmin.shopping.order;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
+@Tag(name = "Orders")
 public class OrderController {
 
     private final OrderService orderService;
@@ -19,12 +23,22 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @Tag(name = "Place an order", description = "Place an order with the supplied products. If some of supplied products does not exist, " +
+            "the order will be created without those products")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "An order was successfully placed")
+    })
     @PostMapping
     public ResponseEntity<OrderSummaryDTO> placeOrder(@RequestBody CreateOrderRequest createOrderRequest) {
         OrderSummaryDTO orderSummaryDTO = orderService.placeOrder(createOrderRequest.email(), createOrderRequest.products());
         return ResponseEntity.ok(orderSummaryDTO);
     }
 
+    @Tag(name = "Get order", description = "Get orders for the given time range.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "At least one order could be found"),
+            @ApiResponse(responseCode = "404", description = "No orders could be found for the specified time range")
+    })
     @GetMapping
     public ResponseEntity<List<OrderSummaryDTO>> getOrderInRange(@RequestParam(name = "from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
                                                        @RequestParam(name = "to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to) {
--- src/main/java/com/kkuzmin/shopping/product/ProductController.java
+++ src/main/java/com/kkuzmin/shopping/product/ProductController.java
@@ -1,5 +1,8 @@
 package com.kkuzmin.shopping.product;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Products")
 public class ProductController {
 
     private final ProductService productService;
@@ -20,12 +24,23 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Tag(name = "Create Product", description = "Create a product with the supplied name, price, currency. " +
+            "If the product with the given name exists, no new product will be created.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Products could be successfully created"),
+            @ApiResponse(responseCode = "400", description = "Product could not be created as there is one with the given name"),
+    })
     @PostMapping
     public ResponseEntity<UUID> createProduct(@RequestBody @UniqueProductConstraint ProductDTO productDTO) {
         UUID productId = productService.createProduct(productDTO);
         return new ResponseEntity<>(productId, HttpStatus.CREATED);
     }
 
+    @Tag(name = "Get all products", description = "Get all available products")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Products found"),
+            @ApiResponse(responseCode = "404", description = "No products found"),
+    })
     @GetMapping
     public ResponseEntity<List<ProductFullDTO>> getAllProducts() {
         List<ProductFullDTO> allProducts = productService.getAllProducts();
@@ -34,6 +49,12 @@ public class ProductController {
                 ResponseEntity.ok(allProducts);
     }
 
+    @Tag(name = "Update a product", description = "Update an existing product with the supplied data, " +
+            "the existing product will be found by supplied 'name'")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product updated successfully"),
+            @ApiResponse(responseCode = "404", description = "No product for the given name found"),
+    })
     @PutMapping
     public ResponseEntity<ProductFullDTO> updateProduct(@RequestBody ProductDTO product) {
         Optional<ProductFullDTO> productOpt = productService.updateProduct(product);
